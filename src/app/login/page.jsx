b1c651fa6b707e231bfaca01340e1d3d61975ca5
abdullahanzar <abdullahanzar789@gmail.com>
2024-06-06 00:59:33 +0530

    feat: login with google implemented

---
+++ src/app/login/page.jsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { createSupabaseBrowserClient } from "@/supabase/browserClient";
+import theme from "@/theme";
+import { Stack } from "@mui/material";
+import { Card } from "@mui/material";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
+const supabase = createSupabaseBrowserClient();
+
+export default function LogIn() {
+  return (
+    <main className="min-h-screen">
+      <Stack
+        direction={"column"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Card
+          sx={{
+            minWidth: { sm: "30%", xs: "80%" },
+            m: { sm: "100px", xs: "50px" },
+            p: "20px",
+            backgroundColor: "black",
+          }}
+        >
+          <Auth
+            supabaseClient={supabase}
+            appearance={{
+              theme: ThemeSupa,
+              variables: {
+                default: {
+                  colors: {
+                    brand: "#9c8a8a",
+                    brandAccent: "#827d7d",
+                  },
+                },
+              },
+            }}
+            providers={["google"]}
+            theme="dark"
+          />
+        </Card>
+      </Stack>
+    </main>
+  );
+}
